ea4b30ee79a381cf597f39c4f82b5530357efad1
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-14 15:01:28 +0900

    feat: emotion 설치 및 설정

---
+++ src/pages/board.tsx
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+function Board() {
+  return (
+    <>
+      <div css={css`color: blue;`}>
+        Board TEST
+      </div>
+    </>
+  );
+}
+
+export default Board;
