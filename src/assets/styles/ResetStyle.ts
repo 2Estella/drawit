1729b33885b1dff77898a86a3eac9dc46e5c1409
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-23 18:01:24 +0900

    chore: resetStyle 변경

--- src/assets/styles/ResetStyle.ts
+++ src/assets/styles/ResetStyle.ts
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 const ResetStyle = css`
   * {margin:0; padding:0; box-sizing: border-box;}
   html, body {overflow-y: hidden; overflow-x: overlay;}
-  html, body, div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, abbr, acronym, address, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, article, aside, hgroup, header, footer, figure, figcaption, nav, section {padding:0; margin:0; box-sizing:border-box; background:transparent;}
+  html, body, div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, abbr, acronym, address, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, article, aside, hgroup, header, footer, figure, figcaption, nav, section {border-collapse: collapse; padding:0; margin:0; box-sizing:border-box; background:transparent;}
   ol, ul, li {list-style:none;}
   label {vertical-align:middle; cursor:pointer;}
   a {text-decoration:none; cursor:pointer; vertical-align:baseline; color:#000;}
