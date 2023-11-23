5dc7e8a92e10823fa07dcbbdd58a61d3a3e05ec0
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-23 18:00:17 +0900

    feat: react-router 패키지 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Global } from '@emotion/react';
-import Board from './pages/Board';
+import DrawBoard from './pages/DrawBoard';
 import ResetStyle from './assets/styles/ResetStyle';
 import GlobalStyle from './assets/styles/GlobalStyle';
 import { socket, SocketContext } from './contexts/WebSocketContext';
+import Lobby from './pages/lobby';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <SocketContext.Provider value={socket}>
-      <div className="App" >
-        <Global styles={[ResetStyle, GlobalStyle]} />
-        <Board />
-      </div>
-    </SocketContext.Provider>
+    <BrowserRouter>
+      <SocketContext.Provider value={socket}>
+        <div className="App" >
+          <Global styles={[ResetStyle, GlobalStyle]} />
+          <Routes>
+            <Route path="/" element={<Lobby />} />
+            <Route path="/drawBoard" element={<DrawBoard />} />
+          </Routes>
+        </div>
+      </SocketContext.Provider>
+    </BrowserRouter>
   );
 }
