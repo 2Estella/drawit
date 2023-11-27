6687908b51a733444f539de889ccebcd4bb5c928
2Estella <96452250+2Estella@users.noreply.github.com>
2023-11-27 18:06:07 +0900

    fix: 방 입장, 목록 출력 관련 수정

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,15 +1,20 @@
 import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { lobbyContainerStyle } from '../assets/styles/pages/LobbyStyles';
+import NicknameModal from '../components/NicknameModal';
 import RoomModal from '../components/RoomModal';
 import { SocketContext } from '../contexts/WebSocketContext';
 
 export default function Lobby() {
   const socket = useContext(SocketContext);
+  const navigate = useNavigate();
 
+  const [isOpenNicknameModal, setIsOpenNicknameModal] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [roomList, setRoomList] = useState([]);
+  const [roomId, setRoomId] = useState('');
 
-  useEffect(() => {
+  const fetchRoomList = () => {
     socket.emit('setInit', { nickname: '', id: socket.id });
 
     socket.on('getRoomList', (rooms) => {
@@ -19,13 +24,45 @@ export default function Lobby() {
 
       setRoomList(newRoomList);
     });
-    console.log(roomList);
+  };
+
+  useEffect(() => {
+    fetchRoomList();
 
     return () => {
       socket.off('getRoomList');
     };
   }, []);
 
+  const enterRoom = (roomId: string, members: number) => {
+    if (members >= 4) {
+      alert('입장할 수 있는 인원을 초과하였습니다.');
+
+      return;
+    }
+
+    setRoomId(roomId);
+
+    const savedNickname = localStorage.getItem('nickname') ?? '';
+
+    if (savedNickname) {
+      localStorage.setItem('roomId', roomId);
+      socket.emit('setNickname', savedNickname);
+      socket.emit('enterRoom', roomId);
+      navigate('/drawBoard');
+
+    } else {
+      setIsOpenNicknameModal(true);
+    }
+  };
+
+  const onCloseNicknameModal = () => {
+    setIsOpenNicknameModal(false);
+
+    localStorage.setItem('roomId', roomId);
+    socket.emit('enterRoom', roomId);
+    navigate('/drawBoard');
+  };
 
   return (
     <div className="LobbyContainer" css={lobbyContainerStyle}>
@@ -40,54 +77,26 @@ export default function Lobby() {
             <tr>
               <th>no.</th>
               <th className='title'>제목</th>
-              <th>방장</th>
               <th>인원</th>
               <th>비고</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>테스트방</td>
-              <td>마스터</td>
-              <td>1/4</td>
-              <td>-</td>
-            </tr>
+            {roomList.length <= 0 && (
+              <tr>
+                <td colSpan={4}>No rooms available</td>
+              </tr>
+            )}
+            {roomList.length > 0 &&
+              roomList.map((room, i) => (
+                <tr key={i} onClick={() => enterRoom(room.roomId, room.members)}>
+                  <td>{i + 1}</td>
+                  <td>{room.roomName}</td>
+                  <td>{room.members}/4</td>
+                  <td>-</td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
@@ -97,6 +106,13 @@ export default function Lobby() {
         height={'250px'}
         isOpen={isOpenModal}
         onClose={() => setIsOpenModal(false)}
+      />
+
+      <NicknameModal
+        width={'350px'}
+        height={'250px'}
+        isOpen={isOpenNicknameModal}
+        onClose={onCloseNicknameModal}
       />
     </div>
   );
