985777c5ce34a8caf80fd4bf7e6d032fe8233bef
2Estella <krsd0501@naver.com>
2023-12-17 16:37:53 +0900

    feat: 배포 관련 패키지 및 작업

--- src/contexts/WebSocketContext.ts
+++ src/contexts/WebSocketContext.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import { io, Socket } from 'socket.io-client';
 
-export const socket: Socket = io('http://localhost:8080');
+export const socket: Socket = io(import.meta.env.VITE_REACT_APP_ELASTIC_IP ?? '');
 export const SocketContext = React.createContext<Socket>(socket);
